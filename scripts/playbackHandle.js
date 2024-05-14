7e0737473478a57620cdac308675695fa31cfb4f
Pavel Asafov <pavelaof@gmail.com>
2024-05-14 14:02:43 +0300

    test play audio with click immitation

--- scripts/playbackHandle.js
+++ scripts/playbackHandle.js
@@ -10,6 +10,14 @@ let source, target, s, t //s,t - first letter of point e.g. A or C etc.
 let playID = null
 let count = 0 // point couner
 
+const testBtn = document.getElementById('test')
+
+testBtn.addEventListener('click', () => {
+  voiceHandle(target.textContent)
+})
+
+console.log(testBtn);
+
 // const points = ['A9', 'D45', 'C13', 'B46', 'C23', 'D39', 'A32', 'B19', 'A31', 'D29', 'A25', 'D18', 'C37', 'B31', 'C17', 'B41', 'A30', 'B29', 'C41', 'D34', 'C34', 'D32', 'C36', 'D24', 'A39', 'D23', 'A41', 'B52', 'C12', 'D56', 'C11', 'D55', 'C9', 'D52', 'C7', 'D51', 'A6', 'B56', 'A4', 'B8', 'A44', 'B30', 'C28', 'B40', 'A43', 'D26', 'C45', 'D17', 'C51', 'D14', 'C44', 'D31', 'A27', 'B21', 'A51', 'D42', 'C24', 'D21', 'C42', 'B43', 'C16', 'D49', 'C18', 'B35', 'C33', 'B23', 'A13', 'B29', 'C30', 'B25', 'C36', 'D22', 'C39', 'D39', 'A17', 'D43', 'A24', 'B45', 'C28', 'D31', 'A28', 'B40', 'A33', 'B17', 'B1', 'C6', 'B46', 'A26', 'D32', 'A36', 'B11', 'C10', 'D54', 'C11', 'B55', 'A46', 'B3', 'D58', 'C2', 'A1']
 const points = ['A9', 'C13', 'D45', 'B46', 'C20']
 
@@ -31,11 +39,14 @@ function playbackHandling() {
   target.textContent = points[count]
   target.classList.add(`point-${points[count].charAt(0).toLowerCase()}`)
 
+  // voiceHandle(target.textContent)
+  // let voice = new Audio('../sounds/A.mp3')
+  // voice.play()
+
+  testBtn.click()
+
   s = source.textContent.charAt(0).toLowerCase()
   t = target.textContent.charAt(0).toLowerCase()
-
-  // передаю текст цели
-  voiceHandle(target.textContent)
 
   arrow.classList.add(`arrow_${s}-${t}`)
 
--- scripts/soundHandle.js
+++ scripts/soundHandle.js
@@ -1,8 +1,22 @@
 export function voiceHandle(target) {
-  let letterAudio = new Audio(`../sounds/${target.charAt(0)}.mp3`)
-  let numberAudio = new Audio(`../sounds/${target.slice(1)}.mp3`)
+  // let letterAudio = new Audio(`../sounds/${target.charAt(0)}.mp3`)
+  // let numberAudio = new Audio(`../sounds/${target.slice(1)}.mp3`)
+  
+  // letterAudio.play()
+  // setTimeout(() => numberAudio.play(), 300)
 
-  letterAudio.play()
-  setTimeout(() => numberAudio.play(), 300)
+  if (target.charAt(0) == 'A') {
+    let letterA = new Audio('../sounds/A.mp3')
+    letterA.play()
+  } else if (target.charAt(0) == 'B') {
+    let letterB = new Audio('../sounds/B.mp3')
+    letterB.play()
+  } else if (target.charAt(0) == 'C') {
+    let letterC = new Audio('../sounds/C.mp3')
+    letterC.play()
+  } else {
+    let letterD = new Audio('../sounds/D.mp3')
+    letterD.play()
+  }
 
 }
