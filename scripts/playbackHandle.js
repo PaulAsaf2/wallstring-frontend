2d1b26e796bacf27828eb06e0d4c8f71b0b7ff61
Pavel Asafov <pavelaof@gmail.com>
2024-05-16 16:55:14 +0300

    refactoring of code playbackHandle

--- scripts/playbackHandle.js
+++ scripts/playbackHandle.js
@@ -1,40 +1,34 @@
 import { playBtn, obj } from '../utils/constants.js'
 import { progressBarHandle } from './progressBarHandle.js'
-import { voiceHandle } from './soundHandle.js'
-const pointA = document.getElementById('point-a')
+import { soundHandle, prepareAudioFiles } from './soundHandle.js'
 const pointB = document.getElementById('point-b')
-const pointC = document.getElementById('point-c')
-const pointD = document.getElementById('point-d')
 const arrow = document.querySelector('.arrow')
 let source, target, s, t //s,t - first letter of point e.g. A or C etc.
 let playID = null
-let count = 0 // point couner
+let index = 0 // index of points array
 let isPlaying = false
-const audioFiles = {}
 
 const points = ['A9', 'D45', 'C13', 'B46', 'C23', 'D39', 'A32', 'B19', 'A31', 'D29', 'A25', 'D18', 'C37', 'B31', 'C17', 'B41', 'A30', 'B29', 'C41', 'D34', 'C34', 'D32', 'C36', 'D24', 'A39', 'D23', 'A41', 'B52', 'C12', 'D56', 'C11', 'D55', 'C9', 'D52', 'C7', 'D51', 'A6', 'B56', 'A4', 'B8', 'A44', 'B30', 'C28', 'B40', 'A43', 'D26', 'C45', 'D17', 'C51', 'D14', 'C44', 'D31', 'A27', 'B21', 'A51', 'D42', 'C24', 'D21', 'C42', 'B43', 'C16', 'D49', 'C18', 'B35', 'C33', 'B23', 'A13', 'B29', 'C30', 'B25', 'C36', 'D22', 'C39', 'D39', 'A17', 'D43', 'A24', 'B45', 'C28', 'D31', 'A28', 'B40', 'A33', 'B17', 'B1', 'C6', 'B46', 'A26', 'D32', 'A36', 'B11', 'C10', 'D54', 'C11', 'B55', 'A46', 'B3', 'D58', 'C2', 'A1']
 // const points = ['A9', 'C13', 'D45', 'B46', 'C20']
-
-document.querySelectorAll('[id^="audio"]').forEach(audio => {
-  audioFiles[audio.id.replace('audio', '')] = audio
-})
 
 source = pointB
 source.textContent = 'B31'
 source.classList.add('point-b')
 
-function playSound(letOrNum) {
-  let audio = audioFiles[letOrNum]
-  if (audio) {
-    audio.muted = false
-    return audio.play()
-  } else {
-    return Promise.reject(`Audio file for ${letOrNum} not found`)
-  }
+function updateArrowDirection() {
+  s = source.textContent.charAt(0).toLowerCase()
+  t = target.textContent.charAt(0).toLowerCase()
+
+  arrow.classList.add(`arrow_${s}-${t}`)
 }
 
-function playbackHandling() {
-  switch (points[count].charAt(0).toLowerCase()) {
+function updateTargetPoint() {
+  const pointA = document.getElementById('point-a')
+  const pointB = document.getElementById('point-b')
+  const pointC = document.getElementById('point-c')
+  const pointD = document.getElementById('point-d')
+
+  switch (points[index].charAt(0).toLowerCase()) {
     case 'a': target = pointA
       break
     case 'b': target = pointB
@@ -44,22 +38,8 @@ function playbackHandling() {
     case 'd': target = pointD
   }
 
-  target.textContent = points[count]
-  target.classList.add(`point-${points[count].charAt(0).toLowerCase()}`)
-
-  s = source.textContent.charAt(0).toLowerCase()
-  t = target.textContent.charAt(0).toLowerCase()
-
-  arrow.classList.add(`arrow_${s}-${t}`)
-
-  let letter = target.textContent.substring(0, 1)
-  let number = target.textContent.substring(1)
-
-  playSound(letter)
-    .then(() => setTimeout(() => playSound(number), 300))
-    .catch(error => alert(error))
-
-  progressBarHandle(count + 1, points.length)
+  target.textContent = points[index]
+  target.classList.add(`point-${points[index].charAt(0).toLowerCase()}`)
 }
 
 export function togglePlay() {
@@ -67,39 +47,37 @@ export function togglePlay() {
     playBtn.classList.add('trigger_btn_pause')
     playBtn.textContent = 'Пауза'
 
-    const audioPromises = Object.values(audioFiles).map(audio => {
-      audio.muted = true
-      return audio.play()
-        .then(() => {
-          audio.pause()
-          audio.currentTime = 0
-        })
-        .catch(error => alert(`Ошибка в подготовке звукогого файла: ${error}`))
-    })
-
-    Promise.all(audioPromises)
+    prepareAudioFiles()
       .then(() => {
-        playbackHandling()
+        updateTargetPoint()
+        updateArrowDirection()
+        soundHandle(target)
+        progressBarHandle(index + 1, points.length)
 
         playID = setTimeout(function play() {
-          count++
-          if (!points[count]) {
+          index++
+          
+          if (!points[index]) {
             playBtn.textContent = 'Старт'
             playBtn.classList.remove('trigger_btn_pause')
             return
           }
-    
+
           source.classList.remove(`point-${s}`)
           arrow.classList.remove(arrow.classList[1])
-    
+
           source = target
-          target = points[count]
-    
-          playbackHandling()
-    
+          target = points[index]
+
+          updateTargetPoint()
+          updateArrowDirection()
+          soundHandle(target)
+          progressBarHandle(index + 1, points.length)
+
           playID = setTimeout(play, obj.delay)
         }, obj.delay)
       })
+      .catch((error) => alert(`В обработке звуковых файлов произошла ошибка: ${error}`))
 
     isPlaying = true
   } else {
--- scripts/soundHandle.js
+++ scripts/soundHandle.js
@@ -1,9 +1,42 @@
-export function voiceHandle(target) {
-  let startPlayPromise = new Audio(`../sounds/${target.charAt(0)}.mp3`)
-  
-  if (startPlayPromise !== undefined) {
-    startPlayPromise.play()
-      .then(() => alert(`play() is success!`))
-      .catch((err) => alert(err))
+const audioFiles = {}
+
+document.querySelectorAll('[id^="audio"]').forEach(audio => {
+  audioFiles[audio.id.replace('audio', '')] = audio
+})
+
+function playSound(letOrNum) {
+  let audio = audioFiles[letOrNum]
+  if (audio) {
+    audio.muted = false
+    return audio.play()
+  } else {
+    return Promise.reject(`Audio file for ${letOrNum} not found`)
   }
+}
+
+export function soundHandle(target) {
+  let letter = target.textContent.substring(0, 1)
+  let number = target.textContent.substring(1)
+
+  playSound(letter)
+    .then(() => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        playSound(number).then(resolve).catch(reject)
+      }, 300)
+    }))
+    .catch(error => alert(`При воспроизведении точки ${letter}${number} произошла ошибка: ${error}.`))
+}
+
+export function prepareAudioFiles() {
+  const audioPromises = Object.values(audioFiles).map(audio => {
+    audio.muted = true
+    return audio.play()
+      .then(() => {
+        audio.pause()
+        audio.currentTime = 0
+      })
+      .catch(error => alert(`Ошибка в подготовке звукогого файла (${audio.id}): ${error}`))
+  })
+
+  return Promise.all(audioPromises)
 }
